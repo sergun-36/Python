941ce4ec5bfe2cc51c27ae527d58aa86a51f2075
sergei <sergun3636@gmail.com>
2021-01-27 22:09:44 +0300

    save changes urls_teleg

--- urls_teleg.py
+++ urls_teleg.py
@@ -1,7 +1,6 @@
-from ..data_test_bot import token
+from data_test_bot import token
 
 url_teleg_updates=f"https://api.telegram.org/bot{token}/getUpdates"
 
 url_teleg_send=f"https://api.telegram.org/bot{token}/sendMessage"
 
-print(data_test_bot.token)
