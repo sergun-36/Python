777a8e6b3606c21ef7989a5cf833d50866118cbd
sergun-36 <sergun_36@mail.ru>
2021-02-03 20:55:30 +0300

    refactor,add key_word_args

--- urls_teleg.py
+++ urls_teleg.py
@@ -1,6 +1,1 @@
-from data_test_bot import token
-
-url_teleg_updates=f"https://api.telegram.org/bot{token}/getUpdates"
-
-url_teleg_send=f"https://api.telegram.org/bot{token}/sendMessage"
-
+root_url="https://api.telegram.org/bot"
